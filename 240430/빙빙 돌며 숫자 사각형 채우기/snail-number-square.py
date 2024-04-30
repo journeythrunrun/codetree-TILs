dac8d152b8462be33185ae6286daf6df7a056847
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-04-30 23:44:49 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240430/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -8,17 +8,17 @@ n,m=map(int,sys.stdin.readline().split())
 
 amap=[ [0]*m for _ in range(n)]
 
-# 시계방향 # 다른 케이스 3-index가능하도록 = 같은 수평선 [1][2]에 있어야함 OR 이 dr에 새수식
+# 시계방향(동~)인 값순서 # 다른 케이스_반대방향: index=3-index가능하기위해, 반대방향짝꿍 [1][2]에 있어야함
 dr=[0,1,0,-1] 
 dc=[1,0,-1,0]
 r,c=0,0
 index=0
 amap[0][0]=1
 for i in range(1,m*n):
-    nr,nc=r+dr[index],c+dc[index] # 기존방향으로 가봄
-    if nr <0 or nc<0 or nr > n-1 or nc > m-1 or amap[nr][nc]!=0:
-        index=(index+1)%4 # 방향업데이트
+    nr,nc=r+dr[index],c+dc[index] # 새 임시위치
+    if nr <0 or nc<0 or nr > n-1 or nc > m-1 or amap[nr][nc]!=0: # 새 임시위치 검사
+        index=(index+1)%4 ## 방향업데이트
     r,c=r+dr[index],c+dc[index] # 이동
     amap[r][c]=i+1
 for a in amap :   
-    print(' '.join(map(str,a)) )
+    print(*a) # M2) print(' '.join(map(str,a)) ) : str로 바꾸기 위해 map하는 O(n) 줄이기 : 저장 때 str()씌워
