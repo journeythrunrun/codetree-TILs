50c89c984dc9e2f3860f57752a346053058e8d8b
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-04-30 23:30:44 +0900

    Title: 문자에 따른 명령 2, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/문자에 따른 명령 2/text-based-commands2.py
+++ 240430/문자에 따른 명령 2/text-based-commands2.py
@@ -1,5 +1,6 @@
+# 1) L : 반시계
+
 x,y=0,0
-# L : 반시계
 
 dx=[1,0,-1,0]
 dy=[0,-1,0,1]
@@ -12,7 +13,7 @@ dire=3
 import sys
 data=sys.stdin.readline()
 for a in data :
-    if a=='L':# 딕셔너리 코드 압축 굳이
+    if a=='L':## 딕셔너리 코드 압축 굳이
         dire =(dire-1)%4
     elif a=='R':
         dire =(dire+1)%4
