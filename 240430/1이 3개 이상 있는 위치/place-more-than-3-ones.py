dfe29b78f6d7bd6a0ff2c15f3c02ca47f8b4bce5
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-04-30 20:31:38 +0900

    Title: 1이 3개 이상 있는 위치, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240430/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,11 +1,11 @@
-# 인접 칸중 1이 적혀있는 칸의 수가 3개 이상인 곳의 개수 
+# 1) 인접 칸중 1이 적혀있는 칸의 수가 3개 이상인 곳의 개수 
+# 2) 쉬워서 이 문젠 설계 안하고 바로 품
+
 import sys
-# n=int(input())
-# print(input().split()) # ['0', '1', '0', '1']
-# amap=[input().split() for _ in range(n)] # [['0']]
 
 n=int(sys.stdin.readline())
-# print(sys.stdin.readline().split()) #['0', '1', '0', '1']
+# split -> 리스트화 ['0', '1', '0', '1']
+## print(sys.stdin.readline().split()) #['0', '1', '0', '1'] # m2) input()도 그럼
 amap=[sys.stdin.readline().split() for _ in range(n)] 
 
 dr=[1,0,-1,0]
@@ -18,7 +18,7 @@ for i in range(n):
             nr, nc= i+dr[k], j+dc[k]
             if nr>=0 and nr<n and nc>=0 and nc<n and amap[nr][nc]=='1':
                 count+=1
-        if count>=3: # 2번 이상 틀리면 탈출로 간략화는 굳이
+        if count>=3: ## 2번 이상 틀리면 탈출로 간략화는 굳이
             result+=1
 
 print(result)
