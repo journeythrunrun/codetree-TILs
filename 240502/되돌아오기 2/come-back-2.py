914980bbf50c71b8b977f49b508d9a8fa34be0fb
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-05-02 18:14:02 +0900

    Title: 되돌아오기 2, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240502/되돌아오기 2/come-back-2.py
@@ -0,0 +1,24 @@
+# 1) 0,0 북방향시작
+# N개의 명령 : R: 시계방향90도, L, F 한칸이동
+# -> 0,0오기까지의 소요 시간 / 못오면 -1
+
+import sys
+data=sys.stdin.readline() # 이번엔 엔터 없네 type
+r,c=0,0
+dr=[0,1,0,-1]
+dc=[1,0,-1,0]
+index=3
+for i in range(len(data)):
+    if data[i]=='L':
+        index=(index-1)%4
+    elif data[i]=='R':
+        index=(index+1)%4
+    else :
+        r+=dr[index]
+        c+=dc[index]
+        if r==0 and c==0: # tap
+            print(i+1)
+            exit()
+    # print(r,c)
+print(-1)
+# 회전에도 1초걸림
