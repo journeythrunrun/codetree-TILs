6c4d8f7e1d64e471ef3a08d2722e73ffecd432ae
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-05-04 13:23:57 +0900

    Title: 컨베이어 벨트, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240504/컨베이어 벨트/conveyor-belt.py
+++ 240504/컨베이어 벨트/conveyor-belt.py
@@ -14,6 +14,12 @@ amap+=list(map(int, sys.stdin.readline().split()))
 
 # 에러 안나네print(amap [1:100])
 # 수식 귀찮다
+# 런타임에러 : 인덱스 인가
+## n을 두번뺼일은 없을텐데. 리스트의 각 요소 한바퀴니까.
+## n==1 
+## t : 1,3 
+## 여러번 바퀴 시간
+t%=(2*n)
 for i in range(2*n):
     if -t+i > 2*n-1 :
         print(amap[-t+i-n],end=' ')
@@ -24,7 +30,7 @@ for i in range(2*n):
 
 
 # print(*amap[-t:-t+n],*amap[],sep='' ) # n개씩 출력 : 수식 좀 귀찮긴하다
-
+# if len( )
 ## 출력
 
 #@ 2차원
