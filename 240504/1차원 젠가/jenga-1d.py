e42481749666a7b01e1230ffdb4b2d097e6bce4a
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-05-04 15:06:53 +0900

    Title: 1차원 젠가, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240504/1차원 젠가/jenga-1d.py
+++ 240504/1차원 젠가/jenga-1d.py
@@ -10,13 +10,13 @@ target_th.append(list(map(int,sys.stdin.readline().split())))  ## '번째'
 # 입력 -> 'int' // list(map(int,
 
 for i in range(2): ## target_th[i] : [2,4] 
-    # m1 : '줄여진 거에서 다시 'n번째'
-    for j in range(  target_th[i][0]-1, target_th[i][1]-1 +1 ) : # 번째라서  ## 이 인덱스 뺀다. # range 오른쪽 끝 포함안되는
-        amap.pop(target_th[i][0]-1) # pop, remove / PR
-        # print('--',j) # pop하면 이미 인덱스가 땡겨져있어버림
+    # m1 : [주의] 한 턴 내에서도 pop하면 이미 인덱스가 땡겨져있어버림 
+    # for j in range(  target_th[i][0]-1, target_th[i][1]-1 +1 ) : # 번째라서  ## 이 인덱스 뺀다. # range 오른쪽 끝 포함안되는
+    #     amap.pop(target_th[i][0]-1) # pop, remove / PR
+    #     # print('--',j) # pop하면 이미 인덱스가 땡겨져있어버림
 
     # m2 위 구간 아닌, 왼쪽 리스트 + 오른쪽 리스트
-    # amap=amap[:target_th[i][0]-1  +1-1 ]+amap[target_th[i][1]-1 +1 : ] 
+    amap=amap[:target_th[i][0]-1  +1-1 ]+amap[target_th[i][1]-1 +1 : ] 
 print(len(amap)) ## 남은 블록 수
 print (*amap, sep='\n') # *변수 & 그 사이들 sep가능
 # - 20m
