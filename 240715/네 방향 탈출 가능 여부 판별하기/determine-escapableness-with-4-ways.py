c23d77df94bb0a4cc9eb084935511a5492247130
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-07-15 20:33:07 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240715/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240715/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -1,7 +1,8 @@
 # 탈출 가능 경로 있는지
 # 뱀없1
 n,m = map(int,input().split())
-amap=[ list(map(int,input().split())) for _ in range(n) ] # 아놔 요런실수 _ 에러 시 사용데이터 다  출력코드?
+amap=[ list(map(int,input().split())) for _ in range(n) ]
+# - 아놔 요런실수 _ 오류 시, 사용데이터 다 일단 출력코드?
 
 from collections import deque
 visited=[[0]*m for _ in range(n)]
@@ -22,10 +23,11 @@ def bfs(v):
         for i in range(4):
             ni,nj=v[0]+di[i],v[1]+dj[i]
             # print(ni,nj)
-            # 먼저
+            # - 인덱스 조건 체크 먼저!!
             if 0<=ni<n and 0<=nj<m and not visited[ni][nj] and amap[ni][nj]==1 :
                     aqueue.append([ni,nj]) # 거리저장 필요 없음
                     visited[ni][nj]=1
     return answer
 print( bfs([0,0])  )  
-# - 23m
+# - 23m  
+#   + 기본 bfs코드 바꿀 때 사소한 부분 수정안한 실수 등? -> 쭉 훑을 때 세밀히 수정
