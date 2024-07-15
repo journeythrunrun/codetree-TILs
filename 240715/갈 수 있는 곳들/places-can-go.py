bafc495d95cd7b8927a530e3e5d9ae60ef1b66a0
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-07-15 21:12:17 +0900

    Title: 갈 수 있는 곳들, Time: 202ms, Memory: 26MB, Status: Passed - CodeTree

--- 240715/갈 수 있는 곳들/places-can-go.py
+++ 240715/갈 수 있는 곳들/places-can-go.py
@@ -1,11 +1,11 @@
 # 0 이동가능 -> 도달 가능 칸 수
 # for 시작지점 -> 주변 미방문 쳌
 
-# 문제 예시로부터 -> 인접 거리1만 이동 아님. 
+# 문제 예시로부터 -> 인접 거리1 초과 이동 가능
+
 from collections import deque
 n, k = map(int,input().split()) # std
 amap=[list(map(int,input().split()) ) for _ in range(n) ] 
-
 starts=[ list(map(int,input().split()) ) for _ in range(k)]
 
 visited=[ [0]*n for _ in range(n) ]
@@ -13,24 +13,21 @@ cnt=0
 
 di=[0,0,1,-1]
 dj=[1,-1,0,0]
-# print(starts)
-## aqueue=deque(starts) # []   ## ueue
-# print(aqueue)
-# print(aqueue.popleft())
-# if 1:
+# m2
+# aqueue=deque(starts) # - []가 요소 밖에 이미 있음  # - 실수하니까 queue보다 que로 
+
 aqueue=deque()
-for a in starts :  # 시작점의 카운팅은
-    a[0]-=1 # 행열 -> -1 # 이건 초반데이터에 대해서 다해주고 시작해야 덜헷갈
+for a in starts :  
+    a[0]-=1 # rc의 인덱스화 -=1  # - 이건 나중보다 초반에 처리해서 저장해주고 시작해야 덜 헷갈림
     a[1]-=1
-    aqueue.append(a) # [ a ]  여야 pop할때 a가 빠짐
+    aqueue.append(a) 
     visited[a[0]][a[1]]=1
 
 if 1:
     while(aqueue):
         a=aqueue.popleft()
-        # 원랜 들어갈때하는데 얘넨 초기값 한번에 넣어줘서
-        # visited[a[0]][a[1]]=1 #여기에 하면 괜찮은 인덱스로 들어간것도 이상해짐
-        cnt+=1 # 중복 체크하고 넣었기에
+        # visited[a[0]][a[1]]=1 # > (다른m수정중에)여기에서 하면 괜찮은 인덱스로 들어간것도 이상해짐
+        cnt+=1 # 중복 체크하고 넣었기에 ㄱㅊ & 원래 뺄때 출력하듯.
         # if not visited[a[0]][a[1]]:
         #     cnt+=1
         #     visited[a[0]][a[1]]=1
@@ -42,4 +39,5 @@ if 1:
 
 print(cnt)
 
-# 21m
+# - 21m -> 오류 29m 
+#   > 문제 조건 세부추가로, 방법 수정 이것저것 하다가 미세하게 놓치곤 함
