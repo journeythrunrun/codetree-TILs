f889045b68f40fd17bcf1dc0a3a60b79ef44113f
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-07-15 21:02:26 +0900

    Title: 갈 수 있는 곳들, Time: 197ms, Memory: 26MB, Status: Passed - CodeTree

--- 240715/갈 수 있는 곳들/places-can-go.py
+++ 240715/갈 수 있는 곳들/places-can-go.py
@@ -14,26 +14,32 @@ cnt=0
 di=[0,0,1,-1]
 dj=[1,-1,0,0]
 # print(starts)
-aqueue=deque(starts) # []   ## ueue
+## aqueue=deque(starts) # []   ## ueue
 # print(aqueue)
 # print(aqueue.popleft())
 # if 1:
+aqueue=deque()
+for a in starts :  # 시작점의 카운팅은
+    a[0]-=1 # 행열 -> -1 # 이건 초반데이터에 대해서 다해주고 시작해야 덜헷갈
+    a[1]-=1
+    aqueue.append(a) # [ a ]  여야 pop할때 a가 빠짐
+    visited[a[0]][a[1]]=1
 
-# for a in starts :  # 시작점의 카운팅은
-#     a[0]-=1 # 행열 -> -1
-#     a[1]-=1
-#     aqueue=deque([a]) # [ a ]  여야 pop할때 a가 빠짐
 if 1:
     while(aqueue):
         a=aqueue.popleft()
-        cnt+=1
+        # 원랜 들어갈때하는데 얘넨 초기값 한번에 넣어줘서
+        # visited[a[0]][a[1]]=1 #여기에 하면 괜찮은 인덱스로 들어간것도 이상해짐
+        cnt+=1 # 중복 체크하고 넣었기에
         # if not visited[a[0]][a[1]]:
         #     cnt+=1
         #     visited[a[0]][a[1]]=1
         for i in range(4):
-            ni, nj = a[0]+di[i]-1,a[1]+dj[i]-1
+            ni, nj = a[0]+di[i],a[1]+dj[i]
             if 0<=ni<n and 0<=nj<n and not visited[ni][nj] and amap[ni][nj]==0:
                 visited[ni][nj]=1
                 aqueue.append([ni,nj])
 
 print(cnt)
+
+# 21m
