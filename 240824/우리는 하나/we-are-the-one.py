15e2486d0508540f96d0d8fc21e25c69c4b1f83c
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-08-24 21:53:56 +0900

    Title: 우리는 하나, Time: 217ms, Memory: 26MB, Status: Passed - CodeTree

--- 240824/우리는 하나/we-are-the-one.py
+++ 240824/우리는 하나/we-are-the-one.py
@@ -6,67 +6,66 @@
 # 시복_k개의 도시 선정 (collection) = n*n C k |  -> n*n=최대 64 
 # --> nC
 
-# 2) amap훑으며 ud 만족하는 개수(bfs로?) 하기엔(min(numbs, k)) 쭉 갈 수 있는거라 다름? 선정된 k에 따라 다름 -> 
+# 2) X_m2. amap훑으며 ud 만족하는 개수(bfs로?) 하기엔(min(numbs, k)) 쭉 갈 수 있는거라 다름? 선정된 k에 따라 다름 -> 
+
 import sys
 inpu=sys.stdin.readline
 n,k,u,d = map(int,inpu().split() )
 amap= [ list(map(int,inpu().split() )) for _ in range(n)]
-# import collections
-from itertools import combinations #
-#~
+
+# - combi
+from itertools import combinations 
 
 dx,dy=[0,0,1,-1], [1,-1,0,0]
 amax=0
-# print(list(range(n*n)) )
-# print(list(combinations( range(n*n) ,k)) ) # 예전꺼라 잘못기억하고 있나 했는데 걍 k가 1이었네 ㅋㅎ [(0,), (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,)]
 
-# - visited & count vc비자
-# - > visited 초기화
+# -   print  (  list   (combinations( range(n*n) ,k)) ) 
+# - [(0,), (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,)]
+#   > 예전꺼라 잘못기억하고 있나 했는데 걍 k가 1이었네 ㅋㅎ 
 
 
-# 큐 전체빠져나올듯
+
+
 from collections import deque
 for selected in combinations(range(n*n),k):
-    # print('sele', selected) # for로 벗겨서 튜플
+    # - 여러 세트에 대한 max값 찾기 때, visited, count : 새로운 세트 마다 초기화
     cnt=0
     visited=[ [False]*n for _ in range(n) ]
 
-    q=deque(selected) # deque([0]) 1개짜리도 이 화 #     # selected=(1,2,3)도
-    #for a in selected : # 2,4,5
-    # combi 시간복잡도
+    q=deque(selected) # (0,) -> deque([0])  튜플은 원래 1개만 있는게 저런 형식으로 있는 거일 뿐임. 형식 특이한 부분 없음  
+    # - selected=(1,2,3) -> deque([1,2,3]) -> pop 
+    
+    # for a in selected : # 2,4,5
     while(q):
-        # print('q',q)
-        a= q.popleft() #~left _queue
-        # print(a)
+        a= q.popleft()
         i,j=a//n, a%n
-        # 출발대상이 여러개이다보니, 이전에 방문했던  곳 방문하지 않도록
+        # - queue에 처음부터 들어있는 값이 여러개인 상황 -> popleft한 것이 방문한 곳이면 continue <-그 두 번째 이후 꺼가, 첫 번째 꺼로 부터 유발된 장소에 또 가지 않도록 ~
         if visited[i][j]==True:
             continue
 
-
-        # print(i,j)    
+        # - visited & count _ vc비자
         cnt+=1
         visited[i][j]=True
-        # print('11111',i,j)
-        # print('q',q)
+
         for k in range(4):
             ni, nj=i+dx[k], j+dy[k]
             if 0<=ni<n and 0<=nj<n and visited[ni][nj]==False and u<=abs(amap[i][j]-amap[ni][nj])<=d:
-                ## cnt+=1 # cnt 두번되네. bfs pop화하면서 주석처리로 바꿨어야
+                # cnt+=1 # - bfs로 바꿨을 땐, while 큐라, 큐입력때든 큐출력때든 한 곳에서만 cnt해야함[cnt 두번됨] # 문제가 연속 이동 가능한 거라 for->bfs_pop으로 바꿀 때, 전체 알고리즘 살펴보며? 이 부분 주석처리로 바꿨어야함.
                 visited[i][j]=True
-                q.append(ni*n+nj)# i j 아니고 ni nj
+                q.append(ni*n+nj)# - > i j 아니고 ni nj
 
-                # print('22222',ni, nj)
-        # print(visited)
-    # print('cnt',cnt)
     amax=max(amax,cnt)
 print(amax)
 
-# u이상 d이하 거꾸로
 # 4) u,d 0 | n 1 | k 1
 
 
 
 # - 멍때림
-# - 28m 틀렸습니다
-#  + 상하좌우 인접한 도시간의 이동만 가능 -> 계속 갈수도..
+# - 1h 3m  (itertools 사용 관련 암기까먹어서 본 시간 뺌)
+#   + 28m 틀렸습니다 : [문제이해] 상하좌우 인접한 도시간의 이동만 가능 -> 이어서 '계속' 못 간다는 말은 없으니 '가능'
+#   + u이상 d이하 거꾸로
+ 
+
+# - O(NCr) 관련 시간복잡도 허가 케이스 _ n최대 8
+#   + 해설_DP_O(C(N,k)*N)_maybeN=n으로 쓰셨던듯 나는 N을 총 개수로 사용, itertools_O(r*C(N,r)_maybe*N_bfs)
