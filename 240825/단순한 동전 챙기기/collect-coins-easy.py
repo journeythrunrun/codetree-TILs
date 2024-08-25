82538b1783ba64274bf14e014941ff0e5e4e4539
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-08-25 20:05:52 +0900

    Title: 단순한 동전 챙기기, Time: 245ms, Memory: 24MB, Status: Passed - CodeTree

--- 240825/단순한 동전 챙기기/collect-coins-easy.py
+++ 240825/단순한 동전 챙기기/collect-coins-easy.py
@@ -13,10 +13,7 @@
 
 # M1_X_숫자들 이동할 때의 최소 거리_두 단계?그래도다름 첫위치부터 첫값까지의도 포함되면 세트다름. -> 걍 전체 경우의 수 셋트 구하자. n도 작으니까 괜찮 ##2,4,7-> 그 첫
 
-
-
-
-#M2 값들 세개 고름. index로. 0,6,7  # - 자세한 변수 형식은 자세한 구현 때가 맞을듯#iabc
+# M2 값들 세개 고름. index로. 0,6,7  # - 자세한 변수 형식은 자세한 구현 때가 맞을듯#iabc
 # - 이동거리 연산 : 거리함수값 다섯개 더하기+=
 #   + 영향시복_변수로는*r이긴한데 r이 3이라 의미없는수준(기타 상수는 의미없음 1...,5) ((n=20))
 #   + [ abs(target[ia][0]-target[ib][0])+abs(target[ia][1]-target[ib][1]) ] # - 어차피 변수이름 다른 거 썼음
@@ -79,45 +76,55 @@ def distance(one, two):
     return abs(one[0] - two[0]) + abs(one[1] - two[1])
 
 
-# - combi 라이브러리순서 : 사용했을 떄 순서 안섞임
-def self_combinations(target, haha):  # r=3처럼 개작은 경우 & # 근데 따로 함수로 만들어서 똑같이 전달하려면(저장꼴) *r못아낌. forforfor내에 하기엔 코드지저분 *r 아끼기도 가능
+# - 라이브러리 함수 순서_combinations  : 사용했을 때 안섞임
+def self_combinations(target, haha):
+    # - r=3처럼 아주 작은 숫자로 고정된 경우 : backtracking을 for문 중첩 으로 빠르게 구현
     temp = []
     for i in range(len(target)):
-        for j in range(i + 1, len(target)):  # i+1. 반복이면 _
+        for j in range(i + 1, len(target)):  # - combi에서 : 반복 안되면 i'+1', 반복되면 'i'. 자리 구별 있 & 반복 있 : 1
             for k in range(j + 1, len(target)):
                 temp.append((target[i], target[j], target[k]))
+                # - 여기에서 리턴하면 한가지 리턴하고 끝나버림 return (target[i], target[j], target[k])
+
     return temp
 
-    # 한가지 리턴하고 끝나네 return (target[i], target[j], target[k])
+# 4)_i
+# - combinations에서 원하는 요청대로 뽑을 게 없는 경우 : [] 리턴 ##print( list(combinations([1,2], 3)) )
 
+# for a in []: # - => 에러 안나고 그냥 for문 바로 탈출
 
-# 4_i
-# print( list(combinations([1,2], 3)) ) # []
-# for a in []: # 에러 안나고 그냥 for 바로 탈출
-#     print('00')
-for aset in self_combinations(target, 3):  # ( [1, 2, 1], [2, 1, 0], [3, 0, 2]
+for aset in self_combinations(target, 3):  ##( [1, 2, 1], [2, 1, 0], [3, 0, 2]
     temp_answer = 0
-    # [인덱스]쓸때 모양 그거 확실한지 생각.
-    temp_answer += distance(se[0], aset[0][1:]) + distance(aset[2][1:], se[1])  # [1,2],
-    temp_answer += distance(aset[0][1:], aset[1][1:]) + distance(aset[1][1:], aset[2][                                                                              1:])  # 한 aset에만 [1:]복붙 놓침실화냐 급해도 요소 하나하나 넘어가듯 봐라라쫌 # 문제 꽤 맞췄어도, 코드논리 틀렸을(,실수) 가능성 꽤됨.
+    # - [인덱스]쓸때 그거에 해당하는 의미, 변수 전체적 모양 그릴 수 있는지. # - aset은 i,j가 1,2위치에 있었음
+    temp_answer += distance(se[0], aset[0][1:]) + distance(aset[2][1:], se[1])  #[1,2],
+    temp_answer += distance(aset[0][1:], aset[1][1:]) + distance(aset[1][1:], aset[2][1:])
+    # - !한 곳의 aset에서만 [1:]복붙 놓침실화냐
+    #   + 급해도 책 읽듯이 술술 읽지 말고, 일부러 눈 힘줘서 느리게 징검다리마냥 요소 하나하나 다 봐라쫌
+    #   + 테스트케이스 꽤 맞췄어도, 코드논리 틀렸을(,실수) 가능성 꽤 됨.
     if answer == 'yet':
         answer = temp_answer
     else:
         answer = min(answer, temp_answer)
+
 if answer == 'yet':
     print(-1)
 else:
     print(answer)
 
 # 4)
-# n=2 -> 3개 못고름 -> -1 나와야함-> 특정케이스보다, 이정도는 논리로 전체케이스 체크
-# 떨어져있는 값_논리뇌체크 = 값sort & 인덱스썼다#이부분굳이테스트케이스로?
-# 아직 미처리한 케이스(answer=-1)뒤에다 적었어야.
+# n=2 -> 3개 못고름 -> -1 나와야함
+#   + -> '특정케이스'보다, 가능한 정도일 땐(이정도는) '논리재검증으로 전체 케이스' 체크 or 뇌로 해당케이스에서의 논리검증 -> 이러면 실수 발견하기 어려우려나. 근데 4)는 특이케이스 처리 단계긴함.
 
-# - nCr은 다항식이나 log,root등으로 표시시 ? 대략? _  & GPT근거수식()
+#   > 떨어져있는 값_논리뇌체크 = 값sort & 인덱스썼다 #이부분굳이테스트케이스로?
+# - ! 아직 미처리한 케이스(answer=-1) : '코드 뒤'에다가 주석했어야함
 
-# - 1h 23m : 공부용 필기를 주석에 했던 시간은 = 끽해봤자 5m되려나
+# - O(nCr)-> product등 최대 O(n*r)
+#   + O(nCr)에서 경우의 수 값 nCr : < [product] n*r(~~for)  # ~~ < O(n*r)
+
+# - 1h 23m
+#   + 문제풀면서 생각 대충 필기 말고,(나중에 결국 이중 반복 작업됨) -> '필기 때 스톱워치 멈추고 제대로 적기' (공부용 필기를 위해 주석에 썼던 시간도 조금 포함됐었음)
 #   + 1h 14m : 8퍼에서 틀림
 #     + 1h 넘은 거라 그냥 틀린 테스트 케이스봄 -> 엣지라기보다 일반적인 케이스 틀렸구나 암
-#   + 전체 논리의 '검증' : 아는 제일 어려운 케이스에서 전체 흐름 출력 체크?
-#     -> temp_answer+=distance(aset[0][1:],aset[1][1:])+distance(aset[1][1:],aset[2][1:])  # 한 aset에만 [1:]복붙 놓침실화냐 급해도 요소 하나하나 넘어가듯 봐라라쫌 # 문제 꽤 맞췄어도, 코드논리 틀렸을(,실수) 가능성 꽤됨.
+
+#     + 전체 논리의 '검증' : '있는 테스트 케이스 중 제일 어려운 케이스에서 전체 흐름 중간출력 체크'
+#     > 한 aset에서만 [1:]복붙 놓침
